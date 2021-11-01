50a3d10ce8c7be9d2af1b54a6bb2b662599d49fa
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-01 13:42:57 -0300

    Currency Implemented. A_Enemies give money on death

---
+++ Assets/Bank/Bank.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour
+{
+    [SerializeField] int startingBalance = 150;
+
+    int currentBalance;
+    public int CurrentBalance {  get { return currentBalance; } }
+
+    private void Awake()
+    {
+        currentBalance = startingBalance;
+    }
+
+    public void Deposit(int amount)
+    {
+        //Returns absolute value of what is passed in to protect from negative inputs
+        currentBalance += Mathf.Abs(amount);
+    }
+
+    public void Withdraw(int amount)
+    {
+        currentBalance -= Mathf.Abs(amount);
+    }
+}
